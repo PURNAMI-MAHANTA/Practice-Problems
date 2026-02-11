b58bfa95b7f435d04218b24c81554494bf9a8f78
PURNAMI-MAHANTA <pareshkumohanta7@gmail.com>
2026-02-12 00:29:42 +0530

    Learn code to write for power of a number

---
+++ Problem-8.py
@@ -0,0 +1,9 @@
+# Write a python program to calculate the square of a number entered by the user.
+a = int(input("Enter number: "))
+
+print("The square of a is: ", a**2 )
+
+# print("The square of a is: ", a^2 ) # This is incorrect for finding square of a number in Python.
+
+print(("The 7th power of a is: ", a**7 ))
+print(("The 23th power of a is: ", a**23 ))
