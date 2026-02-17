a1eea6d728b7ac9eef8dd0a129364f7239859083
PURNAMI-MAHANTA <pareshkumohanta7@gmail.com>
2026-02-18 00:04:04 +0530

    Learn to use 'find' and 'replace' function through an example

---
+++ Problem-12.py
@@ -0,0 +1,6 @@
+# Write a program to detect double space in a string.
+anime = "Demon  Slayer  is my favorite  anime."
+print(anime.find("  ")) #5
+
+# Write a program to replace double space with single space in a string.
+print(anime.replace("  ", " ")) #Demon Slayer is my favorite anime.
