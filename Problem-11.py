5295b9f7e6d4fc84eff2d153dfe3cc4de2d6b12c
PURNAMI-MAHANTA <pareshkumohanta7@gmail.com>
2026-02-17 23:48:35 +0530

    Learn to use find function for detecting spaces

---
+++ Problem-11.py
@@ -0,0 +1,8 @@
+# Write a program to detect space in a string.
+anime = "Demon Slayer"
+print(anime.find(" ")) #5
+
+# Write a program to detect double space in a string.
+anime = "Demon Slayer  "
+print(anime.find("  ")) #12
+
